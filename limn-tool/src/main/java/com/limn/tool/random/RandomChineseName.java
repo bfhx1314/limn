c55976fc376d06cd06a0dc12d2add7060379795a
bfhx1314 <bfhx1314@163.com>
2014-09-28 14:36:41 +0800

    fix

---
+++ limn-tool/src/main/java/com/limn/tool/random/RandomChineseName.java
@@ -0,0 +1,59 @@
+package com.limn.tool.random;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import com.limn.tool.external.ExcelEditor;
+import com.limn.tool.regexp.RegExp;
+
+
+
+public class RandomChineseName {
+
+	/*
+	 * 随机中文姓名使用的变量
+	 */
+	private static ExcelEditor ec = null;
+	private static boolean flag = false;
+	
+	//*********************************************//
+	
+	/**
+	 * 随机获取中文姓名
+	 * @return 姓名
+	 */
+	public static String getChineseName(){
+		if(!flag){
+			init();
+			flag = true;
+		}
+
+		String username = null;
+		boolean isValid = true;
+		
+		while(true){
+			int col = RandomData.getNumberRange(0, 99);
+			int row = RandomData.getNumberRange(0,4999);
+			username = ec.getValue(0, col, row);
+			//判断汉子数
+			ArrayList<String> res = RegExp.matcherCharacters(username, "[\u4e00-\u9fa5]");
+			//判断是否存在非汉子以外字符
+			isValid = !RegExp.findCharacters(username, "[^\u4e00-\u9fa5]");
+			if(res.size()<4 && isValid){
+				break;
+			}
+		}
+		return username;
+	}
+	
+	/**
+	 * 随机姓名初始化
+	 */
+	private static void init(){
+		InputStream is = RandomData.class.getClassLoader().getResourceAsStream(  
+                "data/ChineseName_Data.xls");
+		ec = new ExcelEditor(is);
+	}
+	
+}
+
