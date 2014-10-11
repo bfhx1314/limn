2742cf42eb076de42a0085f985d1721c4d4ea600
bfhx1314 <bfhx1314@163.com>
2014-10-11 20:27:10 +0800

    增加一个wait输出

--- limn-tool/src/main/java/com/limn/tool/common/Common.java
+++ limn-tool/src/main/java/com/limn/tool/common/Common.java
@@ -5,6 +5,7 @@ public class Common {
 	public static void wait(int sec){
 		Long time = ((Integer) sec).longValue();
 		try {
+			Print.log("Wait : " + sec, 0);
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
