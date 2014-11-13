9f98311fa54a214dc23b2ffd291774fad65ba690
snowINsummer <45641783@qq.com>
2014-11-13 10:45:48 +0800

    启动浏览器关键字 增加参数：浏览器类型、URL、IP

--- limn-frame/src/main/java/com/limn/frame/keyword/BaseRunKeyWordImpl.java
+++ limn-frame/src/main/java/com/limn/frame/keyword/BaseRunKeyWordImpl.java
@@ -3,6 +3,7 @@ package com.limn.frame.keyword;
 import com.limn.driver.Driver;
 import com.limn.driver.exception.SeleniumFindException;
 import com.limn.tool.parameter.Parameter;
+import com.limn.tool.regexp.RegExp;
 
 public class BaseRunKeyWordImpl {
 	
@@ -12,21 +13,32 @@ public class BaseRunKeyWordImpl {
 	 * @throws SeleniumFindException
 	 */
 	public static void startBrowser(String[] step) throws SeleniumFindException{
-		if (step.length > 1) {
-			if(step[1].equalsIgnoreCase("firefox")){
-				Parameter.BROWSERTYPE = 1;
-			}else if(step[1].equalsIgnoreCase("ie")){
-				Parameter.BROWSERTYPE = 3;
-			}else if(step[1].equalsIgnoreCase("chrome")){
-				Parameter.BROWSERTYPE = 2;
-			}else{
-				throw new SeleniumFindException("不支持此浏览器类型:" + step[1] + " 支持的类型有:firefox,chrome,ie");
+		int stepLen = step.length;
+		for(int i=1;i<stepLen;i++){
+			if (RegExp.findCharacters(step[i], "(?i)^http|^https")){
+				if (stepLen <= i + 1) {
+					throw new SeleniumFindException("URL地址为空。");
+				}
+				Parameter.URL = step[i] + ":" + step[i + 1];
+				i++;
+			}else if(RegExp.findCharacters(step[i], "(?i)firefox|ie|chrome")){
+				if(step[i].equalsIgnoreCase("firefox")){
+					Parameter.BROWSERTYPE = 1;
+				}else if(step[i].equalsIgnoreCase("ie")){
+					Parameter.BROWSERTYPE = 3;
+				}else if(step[i].equalsIgnoreCase("chrome")){
+					Parameter.BROWSERTYPE = 2;
+				}else{
+					throw new SeleniumFindException("不支持此浏览器类型:" + step[i] + " 支持的类型有:firefox,chrome,ie");
+				}
+			}else if(RegExp.findCharacters(step[i], "^(1\\d{2}|2[0-4]\\d|25[0-5]|[1-9]\\d|[1-9])(\\.(00?\\d|1\\d{2}|2[0-4]\\d|25[0-5][1-9\\d]|\\d)){3}")){
+				Parameter.REMOTEIP = step[i];
 			}
 		}
 		
-		if(step.length > 2){
-			Parameter.URL = step[2];
-		}
+//		if(step.length > 2){
+//			Parameter.URL = step[2];
+//		}
 		startBroswer(Parameter.BROWSERTYPE, Parameter.URL, Parameter.REMOTEIP);
 	}
 	
