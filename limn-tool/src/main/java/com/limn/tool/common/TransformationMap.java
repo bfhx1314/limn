df59be3e008a0e08a8526700ac624bc700ce12f0
bfhx1314 <bfhx1314@163.com>
2014-11-13 18:22:58 +0800

    HashMap转化

---
+++ limn-tool/src/main/java/com/limn/tool/common/TransformationMap.java
@@ -0,0 +1,46 @@
+package com.limn.tool.common;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.limn.tool.regexp.RegExp;
+
+public class TransformationMap {
+	
+	
+	public static String transformationByMap(HashMap<String,String> data){
+		String traString = "HASHMAP\n";
+		for(String key : data.keySet()){
+			if(null == traString){
+				traString = traString + key + "\t" + data.get(key);
+			}else{
+				traString = traString + "\n" + key + "\t" + data.get(key);
+			}
+		}
+		return traString;
+	}
+	
+	public static HashMap<String,String> transformationByString(String data){
+		data = data.substring(9);
+		HashMap<String,String> map = new HashMap<String, String>();
+		String[] dataDe = data.split("\n");
+		for(String key : dataDe){
+			ArrayList<String> keyValue = RegExp.matcherCharacters(key, "[^\t]{1,}");
+			map.put(keyValue.get(0), keyValue.get(1));
+		}
+		return map;
+		
+	}
+	
+	public static void main(String[] args){
+		HashMap<String,String> a = new HashMap<String, String>();
+		a.put("a", "1");
+		a.put("b", "2");
+		String b = TransformationMap.transformationByMap(a);
+		System.out.println(b);
+		HashMap<String,String> c = TransformationMap.transformationByString(b);
+		System.out.println(c.get("a"));
+		System.out.println(c.get("b"));
+	}
+
+}
