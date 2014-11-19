3fbe6ea3b5f9f365f32ada38adac869ce32a1a12
bfhx1314 <bfhx1314@163.com>
2014-11-19 17:09:38 +0800

    少提交文件

---
+++ limn-tool/src/main/java/com/limn/tool/exception/ExcelEditorException.java
@@ -0,0 +1,18 @@
+package com.limn.tool.exception;
+
+public class ExcelEditorException extends Exception{
+	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExcelEditorException(String message) {
+		super(message);
+
+	}
+	
+
+}
