31ca2b20b5cc7abaa62933b8f0139af2b4f9ddab
snowINsummer <45641783@qq.com>
2015-02-02 14:46:40 +0800

    测试报告

--- limn-tool/src/main/java/com/limn/tool/common/FileUtil.java
+++ limn-tool/src/main/java/com/limn/tool/common/FileUtil.java
@@ -1,5 +1,6 @@
 package com.limn.tool.common;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -11,33 +12,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.tools.zip.ZipEntry;
 import org.apache.tools.zip.ZipFile;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 
+import com.limn.tool.parameter.Parameter;
 import com.limn.tool.regexp.RegExp;
 
-
-
 public class FileUtil {
-	
+
 	/**
 	 * 文件是否存在
-	 * @param filePath 文件绝对路径
+	 * 
+	 * @param filePath
+	 *            文件绝对路径
 	 * @return
 	 */
-	public static boolean exists(String filePath){
+	public static boolean exists(String filePath) {
 		return new File(filePath).exists();
 	}
-	
+
 	/**
 	 * 创建文件
-	 * @param filePath 文件绝对路径
+	 * 
+	 * @param filePath
+	 *            文件绝对路径
 	 * @return
 	 */
-	public static void createFile(String filePath){
+	public static void createFile(String filePath) {
 		try {
 			new File(filePath).createNewFile();
 		} catch (IOException e) {
@@ -45,15 +62,18 @@ public class FileUtil {
 			e.printStackTrace();
 		}
 	}
-//	public static void main(String[] args){
-//		createFloder("C:\\aa\\aaa");
-//		createFile("C:\\variable.properties");
-//	}
+
+	// public static void main(String[] args){
+	// createFloder("C:\\aa\\aaa");
+	// createFile("C:\\variable.properties");
+	// }
 	/**
 	 * 清空文件内容
-	 * @param filePath 文件绝对路径
+	 * 
+	 * @param filePath
+	 *            文件绝对路径
 	 */
-	public static void setEmpty(String filePath){
+	public static void setEmpty(String filePath) {
 		File file = new File(filePath);
 		FileWriter fw;
 		try {
@@ -65,59 +85,68 @@ public class FileUtil {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * 获取文件父路径
-	 * @param filePath 文件绝对路径
+	 * 
+	 * @param filePath
+	 *            文件绝对路径
 	 * @return
 	 */
-	public static String getParent(String filePath){
+	public static String getParent(String filePath) {
 		return new File(filePath).getParent();
 	}
-	
+
 	/**
 	 * 获取文件名
-	 * @param filePath 文件绝对路径
+	 * 
+	 * @param filePath
+	 *            文件绝对路径
 	 * @return
 	 */
-	public static String getName(String filePath){
+	public static String getName(String filePath) {
 		return new File(filePath).getName();
 	}
-	
+
 	@SuppressWarnings("resource")
-	public static long getSize(String filePath) throws IOException{
+	public static long getSize(String filePath) throws IOException {
 		File f = new File(filePath);
-        long size=0;
-        if (f.exists()) {
-            FileInputStream fis = null;
-            fis = new FileInputStream(f);
-            size= fis.available();
-        } else {
-            f.createNewFile();
-            System.out.println("文件不存在");
-        }
-        return size;
+		long size = 0;
+		if (f.exists()) {
+			FileInputStream fis = null;
+			fis = new FileInputStream(f);
+			size = fis.available();
+		} else {
+			f.createNewFile();
+			System.out.println("文件不存在");
+		}
+		return size;
 	}
-	
+
 	/**
 	 * 创建文件夹
-	 * @param filePath 文件夹路径
+	 * 
+	 * @param filePath
+	 *            文件夹路径
 	 */
-	public static void createFloder(String filePath){
+	public static void createFloder(String filePath) {
 		new File(filePath).mkdirs();
 	}
-	
+
 	/**
 	 * 读取文件内容
-	 * @param filePath 文件绝对路径（TXT、sql）
+	 * 
+	 * @param filePath
+	 *            文件绝对路径（TXT、sql）
 	 * @return
 	 */
-	public static String readFile(String filePath){
+	public static String readFile(String filePath) {
 		StringBuilder sbr = new StringBuilder();
-		String str ="";
+		String str = "";
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader(filePath));
-			while((str = br.readLine()) != null) {
+			while ((str = br.readLine()) != null) {
 				sbr.append(str + "\n");
 			}
 			br.close();
@@ -128,18 +157,21 @@ public class FileUtil {
 		}
 		return sbr.toString();
 	}
+
 	/**
 	 * 执行每一行sql
-	 * @param filePath 文件绝对路径（TXT、sql）
+	 * 
+	 * @param filePath
+	 *            文件绝对路径（TXT、sql）
 	 * @return
 	 */
-	public static String readFileExecuteSql(String filePath){
+	public static String readFileExecuteSql(String filePath) {
 		StringBuilder sbr = new StringBuilder();
-		String str ="";
+		String str = "";
 		BufferedReader br;
 		try {
 			br = new BufferedReader(new FileReader(filePath));
-			while((str = br.readLine()) != null) {
+			while ((str = br.readLine()) != null) {
 				sbr.append(str + "\n");
 			}
 			br.close();
@@ -150,15 +182,18 @@ public class FileUtil {
 		}
 		return sbr.toString();
 	}
-	
+
 	/**
 	 * 写文件
-	 * @param filePath 文件路径，不存在会新建
-	 * @param str 内容
+	 * 
+	 * @param filePath
+	 *            文件路径，不存在会新建
+	 * @param str
+	 *            内容
 	 */
-	public static void writeFile(String filePath, String str){
+	public static void writeFile(String filePath, String str) {
 		if (!str.isEmpty()) {
-			if (!exists(filePath)){
+			if (!exists(filePath)) {
 				try {
 					new File(filePath).createNewFile();
 				} catch (IOException e) {
@@ -167,86 +202,91 @@ public class FileUtil {
 			}
 			FileWriter fw;
 			try {
-				fw = new FileWriter(filePath,true);
-		        fw.write(str,0,str.length());    
-		        fw.flush();
-		        fw.close();
+				fw = new FileWriter(filePath, true);
+				fw.write(str, 0, str.length());
+				fw.flush();
+				fw.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}	
+		}
 	}
-	
+
 	/**
 	 * 复制文件
-	 * @param sourceFile 源文件
-	 * @param targetFile 目标文件
+	 * 
+	 * @param sourceFile
+	 *            源文件
+	 * @param targetFile
+	 *            目标文件
 	 * @throws IOException
-	 * @throws ParameterException 
+	 * @throws ParameterException
 	 */
-	public static void copyFile(File sourceFile, File targetFile)
-			throws IOException {
+	public static void copyFile(File sourceFile, File targetFile) throws IOException {
 		if (!targetFile.exists()) {
-             File dir = targetFile.getParentFile(); // new
-             if (!dir.exists())
-                     dir.mkdirs();
-             targetFile.createNewFile();
+			File dir = targetFile.getParentFile(); // new
+			if (!dir.exists())
+				dir.mkdirs();
+			targetFile.createNewFile();
 		}
-		if(!sourceFile.exists()){
-//			throw new ParameterException(ParameterException.FILE_NOT_EXIST, sourceFile.getAbsolutePath() + " 文件不存在");
+		if (!sourceFile.exists()) {
+			// throw new ParameterException(ParameterException.FILE_NOT_EXIST,
+			// sourceFile.getAbsolutePath() + " 文件不存在");
 		}
-		InputStream is = null;		
+		InputStream is = null;
 		OutputStream os = null;
 		int len;
-		byte []buff = new byte[1024];
+		byte[] buff = new byte[1024];
 		try {
 			is = new FileInputStream(sourceFile);
 			os = new FileOutputStream(targetFile);
-			while((len = is.read(buff)) != -1){
+			while ((len = is.read(buff)) != -1) {
 				os.write(buff, 0, len);
 			}
 			os.flush();
-		}finally{
-			is.close();			
-			os.close();		
+		} finally {
+			is.close();
+			os.close();
 		}
-		
-//		BufferedInputStream inBuff = null;
-//		BufferedOutputStream outBuff = null;
-//		try {
-//			// 新建文件输入流并对它进行缓冲
-//			inBuff = new BufferedInputStream(new FileInputStream(sourceFile));
-//
-//			// 新建文件输出流并对它进行缓冲
-//			outBuff = new BufferedOutputStream(new FileOutputStream(targetFile));
-//
-//			// 缓冲数组
-//			byte[] b = new byte[1024 * 5];
-//			int len;
-//			while ((len = inBuff.read(b)) != -1) {
-//				outBuff.write(b, 0, len);
-//			}
-//			// 刷新此缓冲的输出流
-//			outBuff.flush();
-//		} finally {
-//			// 关闭流
-//			if (inBuff != null)
-//				inBuff.close();
-//			if (outBuff != null)
-//				outBuff.close();
-//		}
+
+		// BufferedInputStream inBuff = null;
+		// BufferedOutputStream outBuff = null;
+		// try {
+		// // 新建文件输入流并对它进行缓冲
+		// inBuff = new BufferedInputStream(new FileInputStream(sourceFile));
+		//
+		// // 新建文件输出流并对它进行缓冲
+		// outBuff = new BufferedOutputStream(new FileOutputStream(targetFile));
+		//
+		// // 缓冲数组
+		// byte[] b = new byte[1024 * 5];
+		// int len;
+		// while ((len = inBuff.read(b)) != -1) {
+		// outBuff.write(b, 0, len);
+		// }
+		// // 刷新此缓冲的输出流
+		// outBuff.flush();
+		// } finally {
+		// // 关闭流
+		// if (inBuff != null)
+		// inBuff.close();
+		// if (outBuff != null)
+		// outBuff.close();
+		// }
 	}
 
 	/**
 	 * 复制文件夹
-	 * @param sourceDir 源文件夹
-	 * @param targetDir 目标文件夹
+	 * 
+	 * @param sourceDir
+	 *            源文件夹
+	 * @param targetDir
+	 *            目标文件夹
 	 * @throws IOException
 	 */
-	public static void copyDirectiory(String sourceDir, String targetDir)
-			throws IOException {
-		if (!exists(targetDir)){
+	public static void copyDirectiory(String sourceDir, String targetDir) throws IOException {
+		if (!exists(targetDir)) {
 			// 新建目标目录
 			(new File(targetDir)).mkdirs();
 		}
@@ -257,15 +297,13 @@ public class FileUtil {
 				// 源文件
 				File sourceFile = file[i];
 				// 目标文件
-				File targetFile = new File(
-						new File(targetDir).getAbsolutePath() + File.separator
-								+ file[i].getName());
-//				try {
-					copyFile(sourceFile, targetFile);
-//				} catch (ParameterException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
+				File targetFile = new File(new File(targetDir).getAbsolutePath() + File.separator + file[i].getName());
+				// try {
+				copyFile(sourceFile, targetFile);
+				// } catch (ParameterException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
+				// }
 			}
 			if (file[i].isDirectory()) {
 				// 准备复制的源文件夹
@@ -308,49 +346,46 @@ public class FileUtil {
 		}
 
 	}
-	
-	
-	public static String findFileByType(String path, String type){
+
+	public static String findFileByType(String path, String type) {
 		File findFile = new File(path);
-		for(File f:findFile.listFiles()){
+		for (File f : findFile.listFiles()) {
 			String fileName = f.getName();
 			int i = fileName.lastIndexOf(".");
-			if(fileName.substring(i+1).equalsIgnoreCase(type)){
+			if (fileName.substring(i + 1).equalsIgnoreCase(type)) {
 				return f.getAbsolutePath();
 			}
 		}
 		return null;
 	}
-	
-	public static HashSet<String> findFilesByType(String path, String type){
+
+	public static HashSet<String> findFilesByType(String path, String type) {
 		File findFile = new File(path);
 		HashSet<String> set = new HashSet<String>();
-		for(File f:findFile.listFiles()){
+		for (File f : findFile.listFiles()) {
 			String fileName = f.getName();
 			int i = fileName.lastIndexOf(".");
-			if(fileName.substring(i+1).equalsIgnoreCase(type)){
+			if (fileName.substring(i + 1).equalsIgnoreCase(type)) {
 				set.add(fileName);
 			}
 		}
 		return set;
 	}
-	
-	public static HashSet<String> findFiles(String path,boolean isDict){
+
+	public static HashSet<String> findFiles(String path, boolean isDict) {
 		File findFile = new File(path);
 		HashSet<String> set = new HashSet<String>();
-		for(File f:findFile.listFiles()){
-			
-			if(f.isFile()){
+		for (File f : findFile.listFiles()) {
+
+			if (f.isFile()) {
 				set.add(f.getName());
-			}else if(f.isDirectory() == isDict && !RegExp.findCharacters(f.getName(),"^\\.")){
+			} else if (f.isDirectory() == isDict && !RegExp.findCharacters(f.getName(), "^\\.")) {
 				set.add(f.getName());
 			}
 		}
 		return set;
 	}
-	
-	
-	
+
 	/**
 	 * 对于文件内容
 	 * 
@@ -362,13 +397,12 @@ public class FileUtil {
 
 		File file = new File(path);
 
-		if (!file.exists() || file.isDirectory())
-		{
+		if (!file.exists() || file.isDirectory()) {
 			file.getParentFile().mkdirs();
 			file.createNewFile();
-			setFileText(path,"0");
+			setFileText(path, "0");
 		}
-		
+
 		FileInputStream fis = new FileInputStream(file);
 		byte[] buf = new byte[1024];
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -380,14 +414,16 @@ public class FileUtil {
 		fis.close();
 		return baos.toString();
 	}
-	
-	
+
 	/**
 	 * 写入文件内容
-	 * @param path 文件路径
-	 * @param text 内容
+	 * 
+	 * @param path
+	 *            文件路径
+	 * @param text
+	 *            内容
 	 */
-	public static void setFileText(String path, String text){
+	public static void setFileText(String path, String text) {
 		FileOutputStream out = null;
 		try {
 			out = new FileOutputStream(new File(path));
@@ -397,37 +433,138 @@ public class FileUtil {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}   
+		}
 	}
 
 	/**
 	 * 返回文件绝对路径
+	 * 
 	 * @param AbsolutelyPath
 	 * @param path
 	 * @return
 	 */
-	public static String getFileAbsolutelyPath(String AbsolutelyPath,String path){
-		if(RegExp.findCharacters(path, "[/|\\\\]")){
+	public static String getFileAbsolutelyPath(String AbsolutelyPath, String path) {
+		if (RegExp.findCharacters(path, "[/|\\\\]")) {
 			return path;
-		}else{
+		} else {
 			return AbsolutelyPath + "\\" + path;
 		}
 	}
-	
-//	public static String getFileFormatPath(String path){
-//		path = path.replaceAll("//", "/");
-//		path = path.replaceAll("/", "\\");
-//		return path;
-//	}
-//	
-//	
-	
-//	public static void main(String[] args){
-//		System.out.println(RegExp.findCharacters("svn","^\\."));
-//	}
-	
-	
-	
-	
-	
+
+	public static void copySourceToFolder(String sourceFilePath, String filePath, Class<?> clasz) {
+		
+		
+		File file = new File(filePath);
+		
+//		URL resource = clasz.getClass().getResource(sourceFilePath);
+		URL resource = clasz.getClassLoader().getResource(sourceFilePath);
+		URLConnection urlConnection = null;
+		try {
+			urlConnection = resource.openConnection();
+			copyJarResourcesRecursively(file, (JarURLConnection)urlConnection);
+		} catch (IOException e1) {
+			
+		}
+		
+		
+//		
+//		if (!file.exists()) {
+//			// 不存在就将jar包里的ParameterValues.xml复制到指定路径下
+//			File f = new File(Parameter.DEFAULT_TEMP_PATH);
+//			f.mkdirs();
+//
+//			InputStream is = clasz.getClassLoader().getResourceAsStream(sourceFilePath);
+//
+//			try {
+//				saveToFile(filePath, is);
+//			} catch (IOException e) {
+//				Print.log("保存文件异常:" + e.getMessage(), 2);
+//			}
+//		}
+
+
+	}
+
+	/**
+	 * 转换String
+	 * 
+	 * @param is
+	 * @return
+	 * @throws IOException
+	 */
+	public static String inputStream2String(InputStream is) throws IOException {
+		StringBuilder sb1 = new StringBuilder();
+		byte[] bytes = new byte[4096];
+		int size = 0;
+
+		try {
+			while ((size = is.read(bytes)) > 0) {
+				String str = new String(bytes, 0, size, "UTF-8");
+				sb1.append(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb1.toString();
+	}
+
+	/**
+	 * 保存文件
+	 * 
+	 * @param fileName
+	 * @param in
+	 * @throws IOException
+	 */
+	public static void saveToFile(String fileName, InputStream in) throws IOException {
+		FileOutputStream fos = null;
+		BufferedInputStream bis = null;
+		// HttpURLConnection httpUrl = null;
+		// URL url = null;
+		int BUFFER_SIZE = 1024;
+		byte[] buf = new byte[BUFFER_SIZE];
+		int size = 0;
+		//
+		// //建立链接
+		// url = new URL(destUrl);
+		// httpUrl = (HttpURLConnection) url.openConnection();
+		// //连接指定的资源
+		// httpUrl.connect();
+		// //获取网络输入流
+		bis = new BufferedInputStream(in);
+		// //建立文件
+		fos = new FileOutputStream(fileName);
+		//
+		// //保存文件
+		while ((size = bis.read(buf)) != -1)
+			fos.write(buf, 0, size);
+		//
+		fos.close();
+		bis.close();
+		// httpUrl.disconnect();
+	}
+
+	private static void copyJarResourcesRecursively(File destination, JarURLConnection jarConnection) throws IOException {
+		JarFile jarFile = jarConnection.getJarFile();
+		Enumeration<JarEntry> entries = jarFile.entries();
+		while (entries.hasMoreElements()) { // 遍历jar内容逐个copy
+			JarEntry entry = entries.nextElement();
+			if (entry.getName().startsWith(jarConnection.getEntryName())) {
+				String fileName = StringUtils.removeStart(entry.getName(), jarConnection.getEntryName());
+				File destFile = new File(destination, fileName);
+				if (!entry.isDirectory()) {
+					InputStream entryInputStream = jarFile.getInputStream(entry);
+					FileUtils.copyInputStreamToFile(entryInputStream, destFile);
+				} else {
+					FileUtils.forceMkdir(destFile);
+				}
+			}
+		}
+	}
+
 }
